be2fa5734e955ebe6664eb9da7a98823ebde4386
Javiera23 <javiera.munoz01@alumnos.ucn.cl>
2018-09-07 19:12:11 -0300

    Added: - Cotizacion        - Servicio        - Etapa        - Estado

---
+++ Core/Models/Cotizacion.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Core.Models
+{
+    /// <summary>
+    /// Clase que representa una Cotizacion en el sistema de presupuesto.
+    /// </summary>
+    public class Cotizacion : BaseEntity
+    {
+        [Required] public long precio { get; set;}
+        [Required] public DateTime fecha { get; set; }
+        [Required] public List<Servicio> servicios = new List<Servicio>();
+        [Required] public Persona persona { get; set; }
+        [Required] public Estado estado { get; set; }
+        
+        public override void Validate()
+        {
+            if (persona == null)
+            {
+                throw new ModelException("Persona no puede ser null");
+            }
+
+            if (fecha == null)
+            {
+                throw new ModelException("Fecha no puede ser null o vacio");
+            }
+
+            if (precio < 0)
+            {
+                throw new ModelException("El precio no puede ser negativo");
+            }
+            
+            
+        }
+
+
+        
+
+
+    }
+}
---
+++ Core/Models/Servicio.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Core.Models
+{
+    public class Servicio : BaseEntity
+    {
+        [Required] public string nombre { get; set; }
+        [Required] public long precio { get; set; }
+        [Required] public string descripcion { get; set; }
+        [Required] public Etapa etapa { get; set; }
+        
+        
+        public override void Validate()
+        {
+            if (string.IsNullOrEmpty(nombre))
+            {
+                throw new ModelException("El nombre no puede ser null");
+            }
+
+            if (precio < 0)
+            {
+                throw new ModelException("Precio no puede ser negativo");
+            }
+
+            if (string.IsNullOrEmpty(descripcion))
+            {
+                throw new ModelException("La descripcion no puede ser null o estar vacia");
+            }
+                
+           
+        }   
+        
+
+        
+    }
+}
